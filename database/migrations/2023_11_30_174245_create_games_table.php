6b0c1cd7d7eb53022b224773c8b514846c090e2d
AndrésLabat <andreslabat89@gmail.com>
2023-11-30 18:59:56 +0100

    feat: create games and rooms migrations

---
+++ database/migrations/2023_11_30_174245_create_games_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->unique()->require();
+            $table->enum('category', ["action", "RPG", "shooter", "adventure"]);
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('game');
+    }
+};
