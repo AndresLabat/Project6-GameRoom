fa5854392863b98c2b4c2a02c02049af3ea98c7c
AndrésLabat <andreslabat89@gmail.com>
2023-12-01 13:03:49 +0100

    feat: create controllers files

--- routes/api.php
+++ routes/api.php
@@ -37,33 +37,33 @@ Route::post('/logout', [AuthController::class, 'logout']);
 Route::put('/user', [UserController::class, 'updateProfile']);
 Route::delete('/user', [UserController::class, 'deleteUser']);
 
-// GAMES
+// // GAMES
 Route::get('/games', [GameController::class, 'getAllGames']);
 Route::get('/game', [GameController::class, 'getGameById']);
 
-// ROOMS
+// // ROOMS
 Route::get('/room', [RoomController::class, 'getRoomById']);
 Route::post('/room', [RoomController::class, 'createRoom']);
 Route::put('/room', [RoomController::class, 'updateRoom']);
 Route::delete('/delete', [RoomController::class, 'deleteRoom']);
 
-// ROOM_USER
+// // ROOM_USER
 Route::get('/members-room', [Room_userController::class, 'getMembersRoom']);
 Route::get('/rooms-user', [Room_userController::class, 'getRoomsUser']);
-Route::create('/room-user', [Room_userController::class, 'createRoomUser']);
-Route::update('/room-user', [Room_userController::class, 'updateRoomUser']);
+Route::post('/room-user', [Room_userController::class, 'createRoomUser']);
+Route::put('/room-user', [Room_userController::class, 'updateRoomUser']);
 Route::delete('/room-user', [Room_userController::class, 'deleteRoomUser']);
 
-// MESSAGES
+// // MESSAGES
 Route::get('/messages', [MessageController::class, 'roomChat']);
 Route::get('/message', [MessageController::class, 'getMessage']);
 Route::post('/message', [MessageController::class, 'createMessage']);
 Route::put('/message', [MessageController::class, 'updatedMessage']);
 Route::delete('/message', [MessageController::class, 'deleteMessage']);
 
-// SUPER_ADMIN
-Route::create('/game', [Super_adminController::class, 'createGame']);
-Route::update('/game', [Super_adminController::class, 'updateGame']);
+// // SUPER_ADMIN
+Route::post('/game', [Super_adminController::class, 'createGame']);
+Route::put('/game', [Super_adminController::class, 'updateGame']);
 Route::delete('/game', [Super_adminController::class, 'deleteGame']);
 Route::get('/rooms', [Super_adminController::class, 'getAllRooms']);
 Route::get('/users', [Super_adminController::class, 'getAllUsers']);
