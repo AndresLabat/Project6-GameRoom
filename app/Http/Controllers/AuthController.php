8373114e2f7f36449443b9dd40580ea2a41f017e
AndrésLabat <andreslabat89@gmail.com>
2023-12-02 13:32:23 +0100

    feat: created Logout endpoint in AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\PersonalAccessToken;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
@@ -153,7 +154,18 @@ class AuthController extends Controller
         }
     }
 
-
-
-    
+    public function logout(Request $request)
+    {
+        $accessToken = $request->bearerToken();
+        $token = PersonalAccessToken::findToken($accessToken);
+        $token->delete();
+        
+        return response()->json(
+            [
+                "success" => true,
+                "message" => "Logout successfully"
+            ],
+            Response::HTTP_OK
+        );
+    }
 }
