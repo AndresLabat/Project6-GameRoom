180cf631a4a88df6145cbd9b814346f5e0b4a679
AndrésLabat <andreslabat89@gmail.com>
2023-12-02 14:41:33 +0100

    feat: created deleteUser endpoint in UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
-use Laravel\Sanctum\PersonalAccessToken;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
@@ -46,7 +43,7 @@ class UserController extends Controller
             }
 
             $user->save();
-            
+
             return response()->json(
                 [
                     "success" => true,
@@ -63,6 +60,34 @@ class UserController extends Controller
                 [
                     "success" => false,
                     "message" => "Error updating profile user"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    public function deleteUser()
+    {
+        try {
+            
+            $token = auth()->user();
+            User::destroy($token->id);
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "User deleted",
+                ],
+                Response::HTTP_OK
+            );
+
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error deleting user"
                 ],
                 Response::HTTP_INTERNAL_SERVER_ERROR
             );
