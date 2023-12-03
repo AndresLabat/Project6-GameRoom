8c8ab1b9603989d1d4853deab4bed2ce58e7a764
ladronbx <ladronbravovlc@gmail.com>
2023-12-03 20:12:07 +0100

    feat : message control ler roomChat

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
+use App\Models\Room;
+use App\Models\Room_user;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class MessageController extends Controller
 {
-    //
+    public function roomChat(Request $request)
+    {
+        try {
+            
+            $user = auth()->user();
+            $room_id = $request->input('room_id');
+            $user_room = Room_user::query()->where('user_id', $user->id)->where('room_id', $room_id)->first();
+
+            if (!$user_room) {
+                return response()->json(
+                    [
+                        "success" => true,
+                        "message" => "You are not a member of this chat"
+                    ],
+                    Response::HTTP_OK
+                );
+            }
+
+            $roomChat = Message::query()->where('room_id', $room_id)->get();
+
+            if ($roomChat->isEmpty()) {
+                return response()->json(
+                    [
+                        "success" => true,
+                        "message" => "This chat is void"
+                    ],
+                    Response::HTTP_OK
+                );
+            }
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Room chat obtained succesfully",
+                    // "data" =>  $user_room
+                ],
+                Response::HTTP_OK
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error obtaining a chat room"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }
--- app/Http/Controllers/Room_userController.php
+++ app/Http/Controllers/Room_userController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Room;
 use App\Models\Room_user;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class Room_userController extends Controller
