32075ae9c487ec1bc9599d50a2ad97dd0a913704
ladronbx <ladronbravovlc@gmail.com>
2023-12-03 20:50:19 +0100

    feat : added controller createGame in Super_adminController.php

--- app/Http/Controllers/Super_adminController.php
+++ app/Http/Controllers/Super_adminController.php
@@ -3,13 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
-use App\Models\Room;
-use App\Models\Room_user;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
-
 
 class Super_adminController extends Controller
 {
