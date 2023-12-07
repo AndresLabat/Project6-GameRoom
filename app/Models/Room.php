7dd4a96068cc9b6b1f094d8ade6e5803f6c867dd
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-07 17:11:47 +0100

    fix: fix relations between users and rooms

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -1,45 +1,36 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class Room extends Model
 {
     use HasApiTokens, HasFactory, Notifiable;
-
     protected $fillable = [
         'name',
         'game_id',
         'is_active',
         'timestamps',
     ];
-
     public function game(): BelongsTo
     {
         return $this->belongsTo(Game::class);
     }
-
-    public function room_userManyToMany(): BelongsToMany
+    public function room_user()
     {
-        return $this->belongsToMany(User::class, 'room_user');
+        return $this->hasMany(User::class);
     }
-
     public function messageToUser(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'messages');
     }
-
     public function users()
     {
         return $this->belongsToMany(User::class, 'room_user', 'room_id', 'user_id');
     }
-
     public function messages()
     {
         return $this->hasMany(Message::class);
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,19 +1,16 @@
 <?php
-
 namespace App\Models;
-
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
     protected $fillable = [
         'name',
         'nickname',
@@ -23,32 +20,26 @@ class User extends Authenticatable
         'role',
         'timestamps',
     ];
-
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
     public function games(): HasMany
     {
         return $this->hasMany(Game::class);
     }
-
-    public function user_roomManyToMany(): BelongsToMany
+    public function room_user()
     {
-        return $this->belongsToMany(Room::class, 'room_user');
+        return $this->hasMany(Room_user::class);
     }
-
-    public function messageToRoom(): BelongsToMany
-    {
-        return $this->belongsToMany(Room::class, 'messages');
-    }
-
     public function rooms()
     {
         return $this->belongsToMany(Room::class, 'room_user', 'user_id', 'room_id');
     }
-
+    public function messageToRoom(): BelongsToMany
+    {
+        return $this->belongsToMany(Room::class, 'messages');
+    }
     public function messages()
     {
         return $this->hasMany(Message::class);
